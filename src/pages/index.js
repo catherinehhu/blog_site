bbf6845e6fcd83b9ec159fe60aa97aeb1a6fc2e4
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2021-05-20 22:19:10 -0700

    added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,14 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="about" />
-      <h2>about me</h2>
+      <img src = "profile.jpeg" className = "profile"></img>
+      <h5 style = {{textAlign: 'center', marginTop: `1rem`}}>hey, look, it's me!</h5>
+      <h2>who i am</h2>
         <p>Hi there! My name is Catherine Hu, and I'm a computer science and geography student @ UCLA. Welcome to my always-a-work-in-progress site :)</p>
+        <p>I first got into tech near the end of my senior year of high school (right around the beginning of quarantine) because I was drawn to its 
+          potential to build communities and bring people together. I'm excited to use my education and experiences to explore my creativity and passion for social impact.</p>
         <ul><h2>hobbies + interests</h2> 
-        <li>writing short stories</li>
+          <li>writing short stories</li>
           <li>hiking</li>
           <li>stargazing</li>
           <li>coming up with exciting spotify playlist names</li>
