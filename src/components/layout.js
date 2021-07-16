708c1a9cf6f8ce9869d02a631ed1e4972cf6cd13
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2021-07-15 23:36:07 -0700

    linking issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,25 +4,25 @@ import { Link } from "gatsby"
 
 const Layout = ({ location, title, children }) => {
   let home_header = (
-      <Link className="header-link-home" to="/">
+      <Link className="header-link-home" activeClassName = "active-link" to="/">
         {title}
       </Link> 
       )
   let stories_header = (
-        <Link className="header-link-home" to="/stories">
+        <Link className="header-link-home" activeClassName = "active-link" to="/stories">
             stories
         </Link> 
         )
         let experience_header = (
-          <Link className="header-link-home" to="/experience">
+          <Link className="header-link-home" activeClassName = "active-link" to="/experience">
               experience 
           </Link>)
 let photos_header = (
-  <Link className="header-link-home" to="/photos">
+  <Link className="header-link-home" activeClassName = "active-link" to="/photos">
       photos
   </Link> 
   )
-  let resume_header = (<a className = "header-link-home" href="/CatherineHu.pdf">resume</a>)
+  let resume_header = (<a className = "header-link-home" activeClassName = "active-link" href="/CatherineHu.pdf">resume</a>)
   return (
 <div className="global-wrapper" data-is-root-path={"/"}>
       <div className = "header-style">
@@ -35,9 +35,9 @@ let photos_header = (
       </div>
       <main style ={{padding:  `5rem 5rem 0rem`}}>{children}</main>
       <footer>
-      <a href = "https://github.com/catherinehhu"><img src="/githublogo.png" alt="GitHub logo" width="30vh" height="30vh"></img></a>
-        <a href = "https://www.linkedin.com/in/catherinehhu/"><img src="/linkedinlogo.png" alt="LinkedIn logo" width="30vh" height="30vh"></img></a>
-        <a href = "mailto:catherinehuihu@gmail.com"><img src="/gmaillogo.png" alt="Gmail logo" width="30vh" height="30vh"></img></a>
+      <a href = "https://github.com/catherinehhu"><img src="/githublogo.png" alt="GitHub logo" width="40vh" height="40vh"></img></a>
+        <a href = "https://www.linkedin.com/in/catherinehhu/"><img src="/linkedinlogo.png" alt="LinkedIn logo" width="40vh" height="40vh"></img></a>
+        <a href = "mailto:catherinehuihu@gmail.com"><img src="/gmaillogo.png" alt="Gmail logo" width="40vh" height="40vh"></img></a>
         <br></br> 
         <hr></hr>
         <br></br>
